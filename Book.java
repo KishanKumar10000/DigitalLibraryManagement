edb5a2b3f74ce5a3ea9cb0a0d978726947d0ea14
Kishan Kumar <123465936+KishanKumar10000@users.noreply.github.com>
2023-04-25 12:21:54 -0800

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Book {
+
+	public int SNo;
+	public String bookName;
+	public String AuthorName;
+	public int BQuantity;
+	public int BQuantityCopy;
+	String recommendedbooks;
+	String recommendedAuthors;
+
+	Scanner sc = new Scanner(System.in);
+
+	public Book()
+	{
+		System.out.println("Enter Serial No of Book:");
+
+		this.SNo = sc.nextInt();
+		sc.nextLine();
+
+		System.out.println("Enter Book Name:");
+		this.bookName = sc.nextLine();
+
+		System.out.println("Enter Author Name:");
+		this.AuthorName = sc.nextLine();
+
+		System.out.println("Enter Quantity of Books:");
+		this.BQuantity = sc.nextInt();
+		BQuantityCopy = this.BQuantity;
+	}
+
+    public static Book checkOutBook() {
+        return null;
+    }
+}
